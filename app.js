aff43f36ee3ba84274812d4b3914193c8bbce783
gudin10 <carlosandresgudin@gmail.com>
2022-10-26 00:25:57 -0500

    app.js_politic

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ var patientsRouter = require('./routes/patient');
 
 var app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
